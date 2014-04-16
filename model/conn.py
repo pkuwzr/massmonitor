82efc103537bfc1ea1f048bbae274e0a7c3a564c
Jerry King <jerry@mass-virtual-machine.(none)>
2014-04-16 18:54:19 +0800

    ignore the conn.py for database settings

--- model/conn.py
+++ model/conn.py
@@ -1,33 +1,40 @@
+##################################################
+#												 
+#	Author:Jerry King(Wang Zerui),<wzr.cs09@qq.com>		
+#												 
+#	File:conn.py							 
+#	Create Date:2014-04-16 18:46:51					 
+#												 
+##################################################
+
+#!/usr/bin/env python
+#encoding: utf-8
+
+
 #!/usr/bin/env python
 # coding=utf-8
 
 from thing import thing
 
 config = {
-    'db': {
-        'master': {
-                            'url': 'mysql://root:pkuwzr@127.0.0.1:3306/massservices?charset=utf8',
-                            'echo': False,
-                            
-        },
-        'slave': {
-                            'url': 'mysql://root:pkuwzr@127.0.0.1:3306/massservices?charset=utf8',
-                            'echo': False,
-                            
-        },
-                    
-    },
-    'redis': {
-                    'host': 'localhost',
-                    'port': 6379,
-                    'db': 1,
-                    
-    },
-    'thing': {
-                    'debug': True,
-                    
-    }
-            
+	'db': {
+		'master': {
+			'url': 'mysql://root:pkuwzr@127.0.0.1:3306/massservices?charset=utf8',
+			'echo': False,
+		},
+		'slave': {
+			'url': 'mysql://root:pkuwzr@127.0.0.1:3306/massservices?charset=utf8',
+			'echo': False,
+		},
+	},
+	'redis': {
+		'host': 'localhost',
+		'port': 6379,
+		'db': 1,
+	},
+	'thing': {
+		'debug': True,
+	}
 }
 
 thing.Thing.config(config)
